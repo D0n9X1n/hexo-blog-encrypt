a72f71eaea52fefae918c7f50a3a23c45b28f2a3
MikeCoder <mike@mikecoder.cn>
2019-09-02 15:52:33 +0800

    v2.2.4
    1. support password configuration with tag.

--- index.js
+++ index.js
@@ -14,90 +14,76 @@ hexo.extend.filter.register('after_post_render', function encrypt (data) {
 
   // Close the encrypt function
   if (!('encrypt' in hexo.config && hexo.config.encrypt && 'enable' in hexo.config.encrypt && hexo.config.encrypt.enable)) {
-
     return data;
-
   }
+
   if (!('default_template' in hexo.config.encrypt && hexo.config.encrypt.default_template)) { // No such template
-
     hexo.config.encrypt.default_template = fs.readFileSync(path.resolve(__dirname, './template.html'));
-
   }
+
   if (!('default_abstract' in hexo.config.encrypt && hexo.config.encrypt.default_abstract)) { // No read more info
-
     hexo.config.encrypt.default_abstract = 'The article has been encrypted, please enter your password to view.<br>';
-
   }
+
   if (!('default_message' in hexo.config.encrypt && hexo.config.encrypt.default_message)) { // No message
-
     hexo.config.encrypt.default_message = 'Please enter the password to read the blog.';
-
   }
+
   if (!('default_decryption_error' in hexo.config.encrypt && hexo.config.encrypt.default_decryption_error)) { // Wrong password
-
     hexo.config.encrypt.default_decryption_error = 'Incorrect Password!';
-
   }
+
   if (!('default_no_content_error' in hexo.config.encrypt && hexo.config.encrypt.default_no_content_error)) { // No content
-
     hexo.config.encrypt.default_no_content_error = 'No content to display!';
-
   }
-  
+
   var currentTag, needToEncryptTag;
   if (!('password' in data && data.password) && ('tags' in hexo.config.encrypt)) {
     outer:
-    for (var i in data.tags.data) {
-      currentTag = data.tags.data[i].name
-      for (var j in hexo.config.encrypt.tags) {
-        needToEncryptTag = hexo.config.encrypt.tags[j].name
-        if (currentTag == needToEncryptTag) {
-          data.password = hexo.config.encrypt.tags[j].password;
-          break outer;
+    if (data.tags) {
+      for (var i in data.tags.data) {
+        currentTag = data.tags.data[i].name
+        for (var j in hexo.config.encrypt.tags) {
+          needToEncryptTag = hexo.config.encrypt.tags[j].name
+          if (currentTag == needToEncryptTag) {
+            data.password = hexo.config.encrypt.tags[j].password;
+            break outer;
+          }
         }
       }
     }
   }
 
   if ('password' in data && data.password) {
-
     // Use the blog's config first
-    log.info(`Encrypted the blog: ${ data.title.trim() }`);
+    log.info(`Encrypted the blog: ${ data.title.trim() } with password: '${ data.password }'`);
 
     // Store the origin data
     data.origin = data.content;
     data.encrypt = true;
 
     if (!('abstract' in data && data.abstract)) {
-
       data.abstract = hexo.config.encrypt.default_abstract;
-
     }
+
     if (!('template' in data && data.template)) {
-
       data.template = hexo.config.encrypt.default_template;
-
     }
+
     if (!('message' in data && data.message)) {
-
       data.message = hexo.config.encrypt.default_message;
-
     }
+
     if (!('decryptionError' in data && data.decryptionError)) {
-
       data.decryptionError = hexo.config.encrypt.default_decryption_error;
-
     }
+
     if (!('noContentError' in data && data.noContentError)) {
-
       data.noContentError = hexo.config.encrypt.default_no_content_error;
-
     }
 
     if (data.content.trim() === '') {
-
       log.warn('Warning: Your blog has no content, it may cause error when decrypting.');
-
     }
 
     data.content = escape(data.content);
